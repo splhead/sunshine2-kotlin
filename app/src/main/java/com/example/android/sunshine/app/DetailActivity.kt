2a6cb25c53ec37708a34849ab7651cfe102c37d3
splhead <splhead@gmail.com>
2017-10-26 23:51:09 -0400

    3.26

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.kt
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.kt
@@ -3,7 +3,9 @@ package com.example.android.sunshine.app
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.ShareActionProvider
 import android.view.*
 import android.widget.TextView
 
@@ -15,6 +17,7 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_detail)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -25,7 +28,7 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -38,14 +41,43 @@ class DetailActivity : AppCompatActivity() {
     }
 
     class DetailFragment : Fragment() {
+        companion object {
+            val FORECAST_SHARE_HASHTAG: String = " #SunshineApp"
+        }
+
+        var mForecastStr: String = ""
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            setHasOptionsMenu(true)
+            super.onCreate(savedInstanceState)
+        }
+
         override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             val rootView = inflater!!.inflate(R.layout.fragment_detail, container, false)
 
             if (activity.intent != null && activity.intent.hasExtra(Intent.EXTRA_TEXT)) {
                 val forecast = rootView?.findViewById<TextView>(R.id.forecast)
-                forecast?.text = activity.intent.getStringExtra(Intent.EXTRA_TEXT)
+                mForecastStr = activity.intent.getStringExtra(Intent.EXTRA_TEXT)
+                forecast?.text = mForecastStr
             }
             return rootView
+        }
+
+        override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+            inflater?.inflate(R.menu.detailfragment, menu)
+            val menuItem = menu?.findItem(R.id.action_share)
+            val mShareActionProvider = MenuItemCompat.getActionProvider(menuItem) as ShareActionProvider
+
+            mShareActionProvider.setShareIntent(createShareForecastIntent())
+
+        }
+
+        private fun createShareForecastIntent(): Intent {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG)
+            return shareIntent
         }
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.kt
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.android.sunshine.app
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        @JvmField
+        val TAG: String = MainActivity.javaClass.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +43,32 @@ class MainActivity : AppCompatActivity() {
         return if (id == R.id.action_settings) {
             startActivity(Intent(this, SettingsActivity::class.java))
             true
+        } else if (id == R.id.action_map) {
+            openPreferredLocationInMap()
+            true
         } else super.onOptionsItemSelected(item)
 
+    }
+
+    private fun openPreferredLocationInMap() {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        )
+
+        val geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build()
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = geoLocation
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d(TAG, "Couldn't call " + location + ", no receiving apps installed!")
+        }
     }
 
 }
